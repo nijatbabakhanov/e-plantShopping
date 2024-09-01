c80198fe0ca7544b59e9814a65df0a5c9daaa73d
Nijat Babakhanov <nijatbabakhanov@gmail.com>
2024-09-01 16:37:28 +0200

    dispatch error is fixed

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -2,11 +2,14 @@ import React, { useState, useEffect } from "react";
 import "./ProductList.css";
 import CartItem from "./CartItem";
 import { addItem } from "./CartSlice";
+import { useDispatch } from "react-redux";
 
 function ProductList() {
   const [showCart, setShowCart] = useState(false);
   const [showPlants, setShowPlants] = useState(false);
   const [addedToCart, setAddedToCart] = useState({});
+
+  const dispatch = useDispatch();
 
   const plantsArray = [
     {
